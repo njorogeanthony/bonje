0eb07746342af0fe9de255545e30d66f047105ae
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-09 23:03:01 +0300

    Load the auth routes into the application.
    Configure the routes to which unauthenticated and authenticated users will be redirected to

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function (Application $app) {
+            $app->make('router')->name('auth.')->middleware('web')->group(base_path('routes/auth.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('auth.login'));
+        $middleware->redirectUsersTo(fn () => route('dashboard'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
