bfe49e1aed50539ff06ca07c1a9d1bfedb4f4536
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-09 23:01:32 +0300

    Implement the authenticate function to login the user to the app

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->only('username', 'password');
+
+        $request->validate([
+            'username' => 'bail|required|alpha_num',
+            'password' => 'bail|required',
+            'remember' => 'bail|nullable',
+        ]);
+
+        $rememberMe = $request->boolean('remember');
+
+        if (Auth::attempt($credentials, $rememberMe)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'login' => 'The provided credentials do not match our records.',
+        ])->withInput();
+    }
+}
