ff35c5749f21c1146cd15ef6c5c68ebc5b44d86a
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-13 02:12:50 +0300

    Update DatabaseSeeder.php to add users based on environment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Support\Enums\Roles;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,19 +16,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1)->create([
-            'username' => 'admin',
-            'role' => Roles::ADMIN,
-        ]);
+        if (app()->environment('production')) {
+            User::create([
+                'name' => 'John Doe',
+                'email' => 'johndoe@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'username' => 'admin',
+                'role' => Roles::ADMIN,
+            ]);
 
-        User::factory(1)->create([
-            'username' => 'uploader',
-            'role' => Roles::UPLOADER,
-        ]);
+            User::create([
+                'name' => 'Jane Doe',
+                'email' => 'janedoe@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'username' => 'uploader',
+                'role' => Roles::UPLOADER,
+            ]);
 
-        User::factory(1)->create([
-            'username' => 'validator',
-            'role' => Roles::VALIDATOR,
-        ]);
+            User::create([
+                'name' => 'James Doe',
+                'email' => 'jamesdoe@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'username' => 'validator',
+                'role' => Roles::VALIDATOR,
+            ]);
+        } else {
+            User::factory(1)->create([
+                'username' => 'admin',
+                'role' => Roles::ADMIN,
+            ]);
+
+            User::factory(1)->create([
+                'username' => 'uploader',
+                'role' => Roles::UPLOADER,
+            ]);
+
+            User::factory(1)->create([
+                'username' => 'validator',
+                'role' => Roles::VALIDATOR,
+            ]);
+        }
     }
 }
