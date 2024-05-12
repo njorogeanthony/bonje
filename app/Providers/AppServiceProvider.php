caf5bef0418aebf9ad75ab5fadaf49590f5e676e
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-13 00:54:30 +0300

    Add user role macros and blade directives for role-based authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Support\Enums\Roles;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Request::macro('userIsAdmin', function () {
+            return $this->user()->role === Roles::ADMIN;
+        });
+
+        Request::macro('userIsUploader', function () {
+            return $this->user()->role === Roles::UPLOADER;
+        });
+
+        Request::macro('userIsValidator', function () {
+            return $this->user()->role === Roles::VALIDATOR;
+        });
+
+        Blade::directive('isAdmin', fn () => "<?php if (request()->userIsAdmin()): ?>");
+        Blade::directive('endisAdmin', fn () => "<?php endif; ?>");
+
+        Blade::directive('isUploader', fn () => "<?php if (request()->userIsUploader()): ?>");
+        Blade::directive('endisUploader', fn () => "<?php endif; ?>");
+
+        Blade::directive('isValidator', fn () => "<?php if (request()->userIsValidator()): ?>");
+        Blade::directive('endisValidator', fn () => "<?php endif; ?>");
     }
 }
