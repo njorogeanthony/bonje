5af1f55da69919cdc4569567af1acf337ea08d98
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-12 22:27:36 +0300

    Add routes for managing receipts in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ReceiptController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+
+    Route::name("receipts.")->prefix("receipts/")->group(function () {
+        Route::get('', [ReceiptController::class, 'index'])->name('index');
+        Route::view('create', 'app.create')->name('create');
+        Route::post('store', [ReceiptController::class, 'store'])->name('store');
+        Route::get('edit/{receipt}', [ReceiptController::class, 'edit'])->name('edit');
+        Route::put('update/{receipt}', [ReceiptController::class, 'update'])->name('update');
+        Route::delete('destroy/{receipt}', [ReceiptController::class, 'destroy'])->name('destroy');
+    });
 });
