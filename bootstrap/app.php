324ebffb70a56355c8d0005b271b94b7bd777652
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-12 22:25:20 +0300

    Refactor login redirection based on user role in LoginController.php and add support for role enum in User model

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Support\Enums\Roles;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('auth.login'));
-        $middleware->redirectUsersTo(fn () => route('dashboard'));
+        $middleware->redirectUsersTo(function () {
+            if (request()->user()->role === Roles::ADMIN) {
+                return route('dashboard');
+            } elseif (request()->user()->role === Roles::UPLOADER) {
+                return route('receipts.create');
+            }
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
