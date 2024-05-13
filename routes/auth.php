9a72be1064030c07cc2d7dfb87e62279531c86db
Anthony Njoroge <njorogemainaanthony@gmail.com>
2024-05-13 09:42:05 +0300

    Add route to redirect '/' to '/login'

--- routes/auth.php
+++ routes/auth.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('login', 'auth.login')->name('login');
 Route::post('login', [LoginController::class, 'authenticate'])->name('login.post');
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::redirect('/', '/login', 301);
